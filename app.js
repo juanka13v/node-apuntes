a8754b283064af958b39759085623eb5f26271b5
Juan Valero <valerojuan13c@gmail.com>
2021-10-18 20:30:29 -0300

    commit

--- app.js
+++ app.js
@@ -1,53 +1,14 @@
-const http = require('http');
-const {readFileSync} = require('fs');
+const express = require('express');
+const app = express();
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
+app.get('/', (req, res) => {
 
-const server = http.createServer((req, res) => {
-    //console.log(req.url);
-    const url = req.url;
-    // Home Page
-    if(url === '/') {
-        res.writeHead(200,{'content-type':'text/html'});
-        res.write(homePage);
-        res.end(); 
-    }
-    // About Page
-    else if(url === '/about') {
-        res.writeHead(200,{'content-type':'text/html'});
-        res.write('<h1>About Page</h1>');
-        res.end();
-    }
-    // Styles
-    else if(url === '/styles.css') {
-        res.writeHead(200,{'content-type':'text/css'});
-        res.write(homeStyles);
-        res.end();
-    }
-    // image/logo
-    else if(url === '/logo.svg') {
-        res.writeHead(200,{'content-type':'image/svg+xml'});
-        res.write(homeImage);
-        res.end();
-    }
-    // logic
-    else if(url === '/browser-app.js') {
-        res.writeHead(200,{'content-type':'text/javascript'});
-        res.write(homeLogic);
-        res.end();
-    }
-    // 404
-    else {
-        res.writeHead(404,{'content-type':'text/html'});
-        res.write('<h1>Page not Found</h1>');
-        res.end();
-    }
-    
 })
 
+app.all('*', (req, res) => {
+    res.status(404).send('resource not found');
+})
 
-server.listen(5000)
+app.listen(5000, () => {
+    console.log('server is listening on port 5000...');
+})
