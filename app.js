d949a04f29ce9b72e9b2e6622948609997ee670a
Juan Valero <valerojuan13c@gmail.com>
2021-10-21 15:09:19 -0300

    get and post

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const app = express();
-const authorize = require('./authorize');
-const logger = require('./logger');
 
-// req => middleware => res
+let {people} = require('./data');
 
-app.use([logger, authorize]);
+// static assets
 
-app.get('/', (req, res) => {
-    res.send('Home');
+app.use(express.static('./methods-public'));
+
+// parse form data
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/api/people', (req,res) => {
+    res.status(200).json({success:true, data:people});
+});
+
+app.post('/login', (req,res) => {
+    const {name} = req.body;
+
+    if(name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+
+
+    res.status(401).send('Please provide credentials')
 })
 
-app.get('/about', (req, res) => {
-    res.send('About');
-})
 
-app.get('/api/products', (req, res) => {
-    res.send('Products');
-})
-
-app.get('/api/items', (req, res) => {
-    res.send('Items');
-})
-
-
-app.listen(5000, () => {
-    console.log('Server is listening on port 5000...');
-})
+app.listen(3300, () => {
+    console.log('Server is listening on port 3300...');
+});
