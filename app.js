d85a803de7d4e29ad853f39a92c11211ea5ea59b
Juan Valero <valerojuan13c@gmail.com>
2021-10-20 21:14:49 -0300

    middleware

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app = express();
+const authorize = require('./authorize');
+const logger = require('./logger');
 
-const {products} = require('./data.js');
+// req => middleware => res
 
+app.use([logger, authorize]);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+    res.send('Home');
 })
 
-app.get('/api/products', (req,res) => {
-    
+app.get('/about', (req, res) => {
+    res.send('About');
+})
+
+app.get('/api/products', (req, res) => {
+    res.send('Products');
+})
+
+app.get('/api/items', (req, res) => {
+    res.send('Items');
 })
 
 
 app.listen(5000, () => {
-    console.log('server listening on port 5000');
+    console.log('Server is listening on port 5000...');
 })
