5507a092f6204b1e46c20d77c8fb11395544afb0
Juan Valero <valerojuan13c@gmail.com>
2021-10-25 14:59:40 -0300

    end

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-let {people} = require('./data');
+const people = require('./routes/people');
+const auth = require('./routes/auth');
 
 // static assets
-
-app.use(express.static('./methods-public'));
+app.use(express.static("./methods-public"));
 
 // parse form data
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended:false}));
+// parse json
+app.use(express.json());
 
-app.get('/api/people', (req,res) => {
-    res.status(200).json({success:true, data:people});
-});
+app.use('/api/people', people);
 
-app.post('/login', (req,res) => {
-    const {name} = req.body;
+app.use('/login', auth);
 
-    if(name) {
-        return res.status(200).send(`Welcome ${name}`)
-    }
-
-
-    res.status(401).send('Please provide credentials')
-})
 
 
 app.listen(3300, () => {
-    console.log('Server is listening on port 3300...');
+  console.log("Server is listening on port 3300...");
 });
